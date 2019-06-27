df2318a2e663e3d3612505dc4f754615dc659803
ccz <chenchongzuan@baishishuju.cn>
2019-06-27 21:08:37 +0800

    1.devtool 2.html 空白刪除 3.图片压缩比例调整

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,8 @@ const ImageMinPlugin = require('imagemin-webpack-plugin').default
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
-const isLint = true
+const isLint = true  //是否开启ESLint  default:true
+const isSource=false //是否开启dev source源码（断点调试）default:false
 const isProd = process.env.NODE_ENV === 'production'
 const cdnUrl = process.env.VUE_APP_CDN_URL || '/'
 
@@ -26,40 +27,49 @@ module.exports = {
             errors: true
         }
     },
-    configureWebpack: {
-        plugins: [
-            new CopyWebpackPlugin([
-                {
-                    from: path.resolve(__dirname, './static'),
-                    to: 'static',
-                    ignore: ['.*']
+    configureWebpack: (config)=>{
+        return {
+            devtool:isSource&&'eval-source-map'||'eval',
+            plugins: [
+                new CopyWebpackPlugin([
+                    {
+                        from: path.resolve(__dirname, './static'),
+                        to: 'static',
+                        ignore: ['.*']
+                    }
+                ]),
+                new ImageMinPlugin({
+                    disable: !isProd,
+                    pngquant: {
+                        quality: '90-100'
+                    }
+                })
+            ],
+            resolve: {
+                alias: {
+                    '@': resolve('src'),
+                    'static': resolve('static'),
+                    'api': resolve('src/api'),
+                    'assets': resolve('src/assets'),
+                    'components': resolve('src/components'),
+                    'icon': resolve('src/icon'),
+                    'sprite': resolve('src/icon/sprite'),
+                    'layout': resolve('src/layout'),
+                    'mock': resolve('src/mock'),
+                    'styles': resolve('src/styles'),
+                    'utils': resolve('src/utils'),
+                    'views': resolve('src/views')
                 }
-            ]),
-            new ImageMinPlugin({
-                disable: !isProd,
-                pngquant: {
-                    quality: '95-100'
-                }
-            })
-        ],
-        resolve: {
-            alias: {
-                '@': resolve('src'),
-                'static': resolve('static'),
-                'api': resolve('src/api'),
-                'assets': resolve('src/assets'),
-                'components': resolve('src/components'),
-                'icon': resolve('src/icon'),
-                'sprite': resolve('src/icon/sprite'),
-                'layout': resolve('src/layout'),
-                'mock': resolve('src/mock'),
-                'styles': resolve('src/styles'),
-                'utils': resolve('src/utils'),
-                'views': resolve('src/views')
             }
         }
     },
     chainWebpack: config => {
+        if(isProd){
+            config.plugin('html').tap(args => {
+                args[0].minify.collapseWhitespace=false
+                return args
+            })
+        }
         if (process.env.IS_ANALYZ) {
             config.plugin('webpack-report')
                 .use(BundleAnalyzerPlugin, [{
