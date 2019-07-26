f3da6764e6ca7257559647e510859be216543739
ccz <chenchongzuan@baishishuju.cn>
2019-07-26 11:16:09 +0800

    :art:  1.添加分包功能 2.runtimeChunk分割

--- vue.config.js
+++ vue.config.js
@@ -69,6 +69,32 @@ module.exports = {
                 args[0].minify.collapseWhitespace=false
                 return args
             })
+            config.optimization.splitChunks({
+                chunks: 'all',
+                cacheGroups: {
+                    libs: {
+                        name: 'chunk-libs',
+                        test: /[\\/]node_modules[\\/]/,
+                        priority: 10,
+                        chunks: 'initial' // 只打包初始时依赖的第三方
+                    },
+                    vant: {
+                        name: 'chunk-vant', // 单独将 vant 拆包
+                        priority: 20, // 权重要大于 libs 和 app 不然会被打包进 libs 或者 app
+                        test: /[\\/]node_modules[\\/]vant[\\/]/
+                    },
+                    commons: {
+                        name: 'chunk-commons',
+                        test: resolve('src/components'), // 可自定义拓展你的规则
+                        minChunks: 3, // 最小公用次数
+                        priority: 5,
+                        reuseExistingChunk: true
+                    }
+                }
+            })
+            config.optimization.runtimeChunk({
+                name: 'manifest'
+            })
         }
         if (process.env.IS_ANALYZ) {
             config.plugin('webpack-report')
